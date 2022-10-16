ade8b9d0037b78d9083bd84f39719c7b39d728b7
btyee33 <109829125+btyee33@users.noreply.github.com>
2022-10-16 04:58:23 -0500

    Made thank you screen
    yeah

--- screens/ThankYouScreen.js
+++ screens/ThankYouScreen.js
@@ -4,18 +4,33 @@ import { Button, StyleSheet, Text, SafeAreaView } from 'react-native';
 const ThankYouScreen = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.container}>
-            <Text >hello there</Text>
+            <Text style={styles.text}>Thank you for moving someone’s laundry!</Text>
         </SafeAreaView>
     );
   };
 
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff1111',
+      // flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
+      // width: 315,
+      // height: 150,
+      // left: 37,
+      top: 179,
+      borderRadius: 25,
+      backgroundColor: '#B6F0FF',
+      height: 300,
+      margin: 50,
+      alignItems: 'center',
+      justifyContent: 'center'
     },
+    text: {
+      fontSize: 25,
+      textAlign:'center',
+      fontFamily: 'Arial',
+      // fontWeight: 'bold',
+    }
   });
 
 export default ThankYouScreen;
